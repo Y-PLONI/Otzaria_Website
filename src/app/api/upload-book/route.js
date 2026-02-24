f9cc84071d4243095250fa7fa01c3a2d846d22fd
palmoni5 <ff8401948@gmail.com>
2026-02-24 19:50:22 +0200

    הוצאה מהאשפה בהעלאה חדשה

--- src/app/api/upload-book/route.js
+++ src/app/api/upload-book/route.js
@@ -39,6 +39,8 @@ export async function POST(request) {
                 lineCount: content.split('\n').length,
                 uploadType: uploadType, // שימוש בערך שהתקבל או ברירת מחדל
                 status: 'pending',
+                isDeleted: false, // הוצאה מהאשפה אם היה שם
+                deletedAt: null, // איפוס תאריך המחיקה
                 updatedAt: new Date()
             },
             { upsert: true, new: true } // upsert יוצר חדש אם לא נמצא דף תואם
